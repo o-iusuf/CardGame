34e8a132d67bd0cc028da54dcbf90c63f4248220
o-iusuf <31664756+o-iusuf@users.noreply.github.com>
2017-09-10 15:25:29 +0300

    Add files via upload

--- CardGame.java
+++ CardGame.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
 
-public class CardGame {
-
+public abstract class CardGame {
+	private String gameName;
+	private int numPlayersMin;
+	private int numPlayersMax;
+	private int numRoundsMin;
+	private int numRoundsMax;
+	private boolean dealerGame;
+	private ArrayList<Player> playerList;
+	
+	public CardGame(String gameName, int numPlayersMin, int numPlayersMax, int numRoundsMin, int numRoundsMax, boolean dealerGame) {
+		this.gameName = gameName;
+		this.numPlayersMin = numPlayersMin;
+		this.numPlayersMax = numPlayersMax;
+		this.numRoundsMin = numRoundsMin;
+		this.numRoundsMax = numRoundsMax;
+		this.dealerGame = dealerGame;
+	}
+	
+	public boolean addPlayer(String playerName) {
+		boolean addPlayerResult = false;
+		addPlayerResult = this.playerList.add(new Player(playerName));
+		return addPlayerResult;
+	}
+	
+	public boolean removePlayer() {
+		boolean removePlayerResult = false;
+		
+		return removePlayerResult;
+		
+	}
+	
 }
---
+++ Player.java
@@ -0,0 +1,8 @@
+
+public class Player {
+	private String playerName;
+	
+	public Player(String playerName) {
+		this.playerName = playerName;
+	}
+}
